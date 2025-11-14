5d9739e1cf8cc69b7b1cb35c6f0b2a1dcd9ad2c1
Irvin <L22170596@culiacan.tecnm.mx>
2025-11-13 23:49:52 -0700

    Cambio de nombres al servidor

---
+++ EDDO/Backend/back/bdEDDO.py
@@ -0,0 +1,186 @@
+def validarLogin(conexion,correo,contra):
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("""
+            DECLARE @IdDocente INT;
+            EXEC sp_VerificarLoginDocente @Correo = ?, @Contrasena = ?,  @IdDocente = @IdDocente OUTPUT;
+            SELECT @IdDocente AS idDocente;
+        """, (correo, contra))
+        resultado = cursor.fetchone()
+        usuario_id = resultado.idDocente if resultado and resultado.idDocente is not None else None
+
+        return usuario_id
+    except Exception as e:
+        print("❌ Error al consultar nombre de usuario:", e)
+        return None
+    
+def traerExpediente(conexion, idUsuario):
+    try:
+        cursor = conexion.cursor()
+
+        cursor.execute("""
+            SELECT 
+                DOC.NOMBRE AS NOMBRE_DOCUMENTO,
+                DOC.APROBACION
+            FROM DOCUMENTO DOC
+            JOIN DOCUMENTO_EXPEDIENTE DE ON DOC.FOLIO = DE.ID_DOCUMENTO
+            JOIN EXPEDIENTE E ON DE.ID_EXPEDIENTE = E.ID_EXPEDIENTE
+            JOIN DOCENTE D ON E.ID_DOCENTE = D.ID_DOCENTE
+            JOIN CONVOCATORIA C ON E.ID_CONVOC = C.ID_CONVOCATORIA
+            JOIN DEPARTAMENTO DEP ON DOC.ID_DEPARTAMENTO = DEP.ID_DEPARTAMENTO
+            JOIN JEFE J ON DEP.JEFE_ID = J.JEFE_ID
+            JOIN TIPO_DOCUMENTO TD ON DOC.ID_TIPO_DOCUMENTO = TD.ID_TIPO_DOCUMENTO
+            WHERE D.ID_DOCENTE = ?
+        """, (idUsuario,)) 
+
+        filas = cursor.fetchall()
+        if filas:
+            expediente = []
+            for fila in filas:
+                expediente.append({
+                    "Nombre_documento": fila[0],
+                    "APROBACION": fila[1]
+                })
+            return expediente
+        else:
+            return None
+
+    except Exception as e:
+        print("❌ Error al consultar expediente:", e)
+        return None
+
+def traerReclamos(conexion, idUsuario):
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("""
+            SELECT 
+                R.id_reclamo,
+                DOC.NOMBRE AS nombre_documento,
+                DOC.folio,
+                R.fecha
+            FROM DOCUMENTO DOC
+            JOIN DOCUMENTO_EXPEDIENTE DE ON DOC.FOLIO = DE.ID_DOCUMENTO
+            JOIN EXPEDIENTE E ON DE.ID_EXPEDIENTE = E.ID_EXPEDIENTE
+            JOIN DOCENTE D ON E.ID_DOCENTE = D.ID_DOCENTE
+            JOIN CONVOCATORIA C ON E.ID_CONVOC = C.ID_CONVOCATORIA
+            JOIN DEPARTAMENTO DEP ON DOC.ID_DEPARTAMENTO = DEP.ID_DEPARTAMENTO
+            JOIN JEFE J ON DEP.JEFE_ID = J.JEFE_ID
+            JOIN TIPO_DOCUMENTO TD ON DOC.ID_TIPO_DOCUMENTO = TD.ID_TIPO_DOCUMENTO
+            JOIN RECLAMO R ON DOC.FOLIO = R.ID_DOCUMENTO
+            where D.ID_DOCENTE = ? or J.JEFE_ID = ?
+        """, (idUsuario, idUsuario)) 
+
+        filas = cursor.fetchall()
+
+        if filas:
+            reclamos = []
+            for fila in filas:
+                id_reclamo, nombre_doc, folio, fecha = fila  # Desempaqueta la tupla
+                if id_reclamo or fecha or nombre_doc or folio:
+                    reclamos.append({
+                        "id_reclamo": id_reclamo,
+                        "nombre_documento": nombre_doc,
+                        "folio": folio,
+                        "fecha": fecha
+                    })
+            return reclamos
+        else:
+            return []
+
+    except Exception as e:
+        print("❌ Error al consultar reclamos:", e)
+        return None
+    
+def cambiarContra(conexion, correo, nuevaContra):
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("""
+            EXEC sp_CambiarContrasenaDocente @Correo = ?, @NuevaContrasena = ?
+        """, (correo, nuevaContra))
+        conexion.commit()
+        return True
+    except Exception as e:
+        print("❌ Error al cambiar la contraseña:", e)
+        return False
+    
+def cambiarContraActual(conexion, idUsuario, contraNueva, contraActual):
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("SELECT CONTRA FROM DOCENTE WHERE ID_DOCENTE = ?", (idUsuario,))
+        resultado = cursor.fetchone()
+
+        if not resultado:
+            return {"estatus": False, "error": "No se encontró el docente."}
+
+        contraGuardada = resultado[0]
+
+        if contraGuardada != contraActual:
+            return {"estatus": False, "error": "La contraseña actual no coincide."}
+
+        cursor.execute("""
+            EXEC sp_ActualizarContrasenaDocente 
+                @IdDocente = ?, 
+                @NuevaContrasena = ?
+        """, (idUsuario, contraNueva))
+        conexion.commit()
+
+        return {"estatus": True, "error": None}
+
+    except Exception as e:
+        print("❌ Error al cambiar la contraseña actual:", e)
+        return {"estatus": False, "error": str(e)}
+
+    finally:
+        try:
+            cursor.close()
+        except:
+            pass
+
+def guardarMensaje(conexion, idUsuario, idReclamo,mensaje):
+    try:
+        cursor = conexion.cursor()
+        if idUsuario:
+            if int(idUsuario) >= 1000:
+                remitente = "JEFE"
+            else:
+                remitente = "DOCENTE"
+        cursor.execute("""
+            INSERT INTO COMENTARIOS (ID_RECLAMO, DESCRIPCION, FECHA, REMITENTE)
+            VALUES (?, ?, GETDATE(),?)
+        """, (idReclamo, mensaje, remitente))
+        conexion.commit()
+        return True
+    except Exception as e:
+        print("❌ Error al guardar el mensaje:", e)
+        return False
+    
+def traerMsjs(conexion, nombreDoc, idUsuario):
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("""
+            SELECT 
+                CO.remitente,
+                CO.fecha,
+                CO.descripcion
+            FROM DOCUMENTO DOC
+            JOIN DOCUMENTO_EXPEDIENTE DE ON DOC.FOLIO = DE.ID_DOCUMENTO
+            JOIN EXPEDIENTE E ON DE.ID_EXPEDIENTE = E.ID_EXPEDIENTE
+            JOIN DOCENTE D ON E.ID_DOCENTE = D.ID_DOCENTE
+            JOIN CONVOCATORIA C ON E.ID_CONVOC = C.ID_CONVOCATORIA
+            JOIN DEPARTAMENTO DEP ON DOC.ID_DEPARTAMENTO = DEP.ID_DEPARTAMENTO
+            JOIN JEFE J ON DEP.JEFE_ID = J.JEFE_ID
+            JOIN TIPO_DOCUMENTO TD ON DOC.ID_TIPO_DOCUMENTO = TD.ID_TIPO_DOCUMENTO
+            JOIN RECLAMO R ON DOC.FOLIO = R.ID_DOCUMENTO
+            JOIN COMENTARIOS CO ON CO.ID_RECLAMO = R.ID_RECLAMO
+            WHERE (D.ID_DOCENTE = ? AND R.id_Reclamo = ?) 
+            OR (J.JEFE_ID   = ? AND R.id_Reclamo = ?)
+        """, (idUsuario, nombreDoc, idUsuario, nombreDoc))
+
+        
+        filas = cursor.fetchall()
+        return filas if filas else None
+
+    except Exception as e:
+        print("❌ Error al traer los mensajes:", e)
+        return False
+
---
+++ EDDO/Backend/back/sevidor.py
@@ -0,0 +1,262 @@
+from flask import Flask, request, jsonify
+import smtplib, random
+from email.message import EmailMessage
+from flask_cors import CORS
+from bdEDDO import validarLogin, traerExpediente, traerReclamos, cambiarContra,cambiarContraActual,guardarMensaje,traerMsjs
+from bdTec import  traerEmpleados
+
+import pyodbc
+app = Flask(__name__)
+CORS(app)
+
+def conectar_bd(bd):
+    try:
+        conexion = pyodbc.connect(
+            f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+            f'SERVER=localhost;'
+            f'DATABASE={bd};'
+            f'UID=irvin;'
+            f'PWD=123;'
+        )
+        return conexion
+    except Exception as e:
+        print("❌ Error al conectar a la base de datos:", e)
+        return None
+    
+@app.route("/enviar-codigo", methods=["POST"])
+def enviar_codigo():
+    data = request.get_json()
+    correo_destino = data.get("correo")
+
+    if not correo_destino:
+        return jsonify({"ok": False, "error": "No se proporcionó correo"}), 400
+
+    codigo = random.randint(100000, 999999)
+    remitente = "vevovevo963@gmail.com"
+    contraseña = "rryeyeztaugrbppy"
+
+    msg = EmailMessage()
+    msg["Subject"] = "Código de verificación"
+    msg["From"] = remitente
+    msg["To"] = correo_destino
+    msg.set_content(f"Tu código de verificación es: {codigo}")
+
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(remitente, contraseña)
+            smtp.send_message(msg)
+        return jsonify({"estatus": True, "mensaje": "Correo enviado correctamente", "codigo": codigo})
+    except Exception as e:
+        return jsonify({"estatus": False, "error": str(e)}), 500
+
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    correo = data.get("correo")
+    contra = data.get("contra")
+    
+    if not correo or not contra:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    docente_id = validarLogin(conexion, correo, contra)
+    conexion.close()
+
+    if docente_id:
+        return jsonify({"estatus": True, "id_docente": docente_id})
+    else:
+        return jsonify({"estatus": False, "error": "Credenciales inválidas"}), 401
+    
+
+@app.route("/expedient", methods=["POST"])
+def expediente():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+
+    if not idUsuario:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    expediente = traerExpediente(conexion, idUsuario)
+    conexion.close()
+
+    if expediente:
+        return jsonify({"estatus": True, "expediente": expediente})
+    else:
+        return jsonify({"estatus": False, "error": "No se encontró expediente"}), 404
+
+@app.route("/reclamos", methods=["POST"])
+def reclamos():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+
+    if not idUsuario:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+    
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+    
+    reclamos = traerReclamos(conexion, idUsuario)
+    conexion.close()
+    
+    if reclamos:
+        return jsonify({"estatus": True, "reclamos": reclamos})
+    else:
+        return jsonify({"estatus": False, "error": "No se encontraron reclamos"}), 404
+    
+
+@app.route("/cuenta", methods=["POST"])
+def cuenta():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+
+    if not idUsuario:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("BDTEC")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    cuenta = traerEmpleados(conexion, idUsuario)
+    conexion.close()
+
+    if cuenta:
+        return jsonify({"estatus": True, "cuenta": cuenta})
+    else:
+        return jsonify({"estatus": False, "error": "No se encontraron datos de la cuenta"}), 404 
+    
+@app.route("/verificarCorreo", methods=["GET"])
+def verificarCorreo():
+    correo = request.args.get("correo")
+
+    if not correo:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+    
+    cursor = conexion.cursor()
+    cursor.execute("SELECT COUNT(*) FROM Docente WHERE correo = ?", (correo,))
+    resultado = cursor.fetchone()
+    conexion.close()
+
+    if resultado and resultado[0] > 0:
+        return jsonify({"estatus": True})
+    else:
+        return jsonify({"estatus": False})
+
+    
+@app.route("/cambiar-contrasena", methods=["POST"])
+def cambiarContrasena():
+    data = request.get_json()
+    docente_id = data.get("correo")
+    contraNueva = data.get("contraNueva")
+
+    if not docente_id or not contraNueva:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    cuenta = cambiarContra(conexion, docente_id, contraNueva)
+    conexion.close()
+
+    if cuenta:
+        return jsonify(True)
+    else:
+        return jsonify(False), 404 
+    
+@app.route("/cambiarContraActual", methods=["POST"])
+def cambiarContraseñaActual():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+    contraActual = data.get("contraActual")
+    contraNueva = data.get("contraNueva")
+
+    if not contraNueva or not contraActual:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    respuesta = cambiarContraActual(conexion, idUsuario, contraActual,contraNueva)
+    conexion.close()
+
+    if respuesta["estatus"]:
+        return jsonify({"estatus": True})
+    else:
+        return jsonify({"estatus": False, "error": respuesta["error"]}), 401
+    
+@app.route("/guardar-mensaje", methods=["POST"])
+def guardarMsj():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+    idReclamo= data.get("idReclamo")
+    mensaje = data.get("mensaje")
+
+    if not idUsuario or not mensaje:
+        return jsonify({"estatus": False, "error": "Faltan datos"}), 400
+
+    conexion = conectar_bd("EDDO")
+    if not conexion:
+        return jsonify({"estatus": False, "error": "No se pudo conectar a la base de datos"}), 500
+
+    respusta = guardarMensaje(conexion, idUsuario, idReclamo , mensaje);
+    conexion.close()
+    if respusta:
+        return jsonify({"estatus": True})
+    else:
+        return jsonify({"estatus": False, "error": "No se pudo guardar el mensaje"}), 500
+    
+from flask import jsonify, request
+
+@app.route("/traer-mensajes", methods=["POST"])
+def traerMensajes():
+    data = request.get_json()
+    idUsuario = data.get("idUsuario")
+    nombreDoc = data.get("nombreDoc")
+
+    try:
+        conexion = conectar_bd("EDDO")
+        if not conexion:
+            return jsonify({"error": "No se pudo conectar a la base de datos"}), 500
+
+        filas = traerMsjs(conexion, nombreDoc, idUsuario)
+
+        if filas:
+            mensajes = []
+            for fila in filas:
+                remitente, fecha, descripcion = fila
+                mensajes.append({
+                    "remitente": remitente,
+                    "fecha": fecha.strftime("%Y-%m-%d %H:%M:%S"),
+                    "descripcion": descripcion
+                })
+            return jsonify({"estatus":True,"msjs":mensajes})
+        else:
+            return jsonify([])
+
+    except Exception as e:
+        print("❌ Error al consultar mensajes:", e)
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+
